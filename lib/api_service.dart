a9ad9d03f34b93126b1222aba39f3ee62b147ab8
Andres felipe duque caro <ing.proyectos@saitempsa.com>
2026-04-15 17:06:20 -0500

    feat: endpoint global

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -3,7 +3,8 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  final String baseUrl = "https://tu-api.com/api"; // Reemplaza con tu URL real
+  // CONFIGURACIÓN GLOBAL: Cambia esta URL para apuntar a tu servidor
+  static const String baseUrl = "http://10.0.2.2:8000/api"; // 10.0.2.2 es el localhost para el emulador de Android
 
   // --- Manejo del Token ---
 
